024b079bd72e889afd1b87ae66487d2597e635b3
Max Kersten <max-kersten@live.nl>
2018-11-29 02:35:41 +0100

    [+] StarSystem

---
+++ CityGen/Assets/Scrips/Planet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+    bool freq64;
+    AudioPeer AP;
+    public Renderer RD;
+    public int freqBand;
+    public Color color;
+
+    private void Start() {
+        RD.material.SetColor("_TintColor", color);
+        AP = AudioPeer.Instance;
+        RD = this.GetComponent<Renderer>();
+    }
+
+    private void Update() {
+        RD.material.SetColor("_EmissionColor", new Color(AP._audioBandBuffer[freqBand] *2, AP._audioBandBuffer[freqBand] *2, AP._audioBandBuffer[freqBand] *2));
+        transform.localScale = new Vector3(AP._audioBandBuffer[freqBand]/20, AP._audioBandBuffer[freqBand]/20, AP._audioBandBuffer[freqBand]/20);
+    }
+}
